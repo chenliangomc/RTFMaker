e6e02e28c5b029dcf5baeb5d6f918f5539a70317
Liang Chen <liangchenomc@gmail.com>
2019-08-14 15:06:26 -0400

    declare public interface

--- RTFMaker/core.py
+++ RTFMaker/core.py
@@ -22,6 +22,76 @@ from __future__ import absolute_import
 class RTFDocument(object):
     """RTF document container"""
 
+    DEFAULT_LANGUAGE = 'EnglishUS'
+
+    def __init__(self, **kwargs):
+        self._element_cache = list()
+        self._default_p_style = None
+        self._doc = None
+
+    def append(self, element):
+        """add new element to the document"""
+        self._element_cache.append(element)
+        return None
+
+
+    def _collect_styles(self, **kwargs):
+        """get all the registered styles
+
+        return `PyRTF.Elements.StyleSheet`
+        """
+
+    def _collect_elements(self, **kwargs):
+        """get all the elements
+
+        return `PyRTF.document.section.Section`
+        """
+
+    def _to_rtf(self, **kwargs):
+        """convert internal representation of document structure into RTF stream
+
+        return `PyRTF.Elements.Document`
+        """
+        from PyRTF.Constants import Languages
+        from PyRTF.Elements import Document
+
+        # create document object, and capture all the styles;
+        _doc = Document(
+            style_sheet=self._collect_styles(**kwargs),
+            default_language=getattr(Languages, self.DEFAULT_LANGUAGE),
+        )
+
+        return _doc
+
+    def _write(self, file, **kwargs):
+        """dump the full document into the file"""
+        if not self._doc:
+            self._doc = self._to_rtf(**kwargs)
+
+        return self._doc.write(file, **kwargs)
+
+    def to_string(self, **kwargs):
+        """
+        return the string stream of the full document
+
+        @param strip_newline:
+        @type strip_newline: boolean
+
+        return `string`
+        """
+
+        from StringIO import StringIO
+        cache = StringIO()
+        self._write(cache, **kwargs)
+        ret = cache.getvalue()
+        return ret
+
+    def __repr__(self):
+        ret = "<RTF document of {ec} element(s) at {addr}>".format(
+            ec=len(self._element_cache),
+            addr="0x%x"%(id(self)),
+        )
+        return ret
 
 
 #---eof---#
--- readme.md
+++ readme.md
@@ -3,6 +3,41 @@ RTFMaker
 
 A simple RTF document generation package
 
+Quick Start
+-----------
+
+```python
+from RTFMaker import RTFDocument
+
+r = RTFDocument()
+
+cache = [
+
+    {
+        'type':'paragraph',
+        'value': 'a simple line in this document.',
+        'append_newline': True,
+    },
+    {
+        'type':'paragraph',
+        'value': 'This is a line that has the style is different than the default style: bold.',
+        'append_newline': True,
+        'font': 'font-family:Arial;font-weight:bold;font-size:10pt;',
+    },
+    {
+        'type':'paragraph',
+        'value': 'This is a line that has the style is different than the default style: italic.',
+        'append_newline': True,
+        'font': 'font-family:Arial;font-style:italic;font-size:10pt;',
+    },
+]
+
+for i in cache:
+    r.append(i)
+
+print r.to_string()
+```
+
 License
 -------
 

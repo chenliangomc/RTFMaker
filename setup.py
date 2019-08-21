ae5af91f667ceacd573a6753e1105e022dbd71e1
Liang Chen <liangchenomc@gmail.com>
2019-08-21 15:35:20 -0400

    bump version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3
 
 from setuptools import setup, find_packages
 
-VERSION = "0.1.2"
+VERSION = "0.1.3"
 
 setup(
     name='RTFMaker',
